a9941966d6ba8647b628aea51740e9a49a3bbf74
kdc119 <skt32@naver.com>
2023-11-22 10:15:49 +0900

    1st

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,7 +2,7 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractConst = "0xBD7A1cdbA67495ddb93C7944c295E5678B5a8267";
 
 // The name of the chain your contract is deployed to.
 // Refer to README.md on how to specify the chain name.
@@ -12,7 +12,7 @@ export const chainConst = "mumbai";
 // Please ensure that you define the correct domain for your API key from the API settings page.
 // You can get one for free at https://thirdweb.com/create-api-key
 // Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
-export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "";
+export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "d269977fda7aca6c7c1a520b503e9610";
 
 // Configure the primary color for buttons and other UI elements
 export const primaryColorConst = "blue";
